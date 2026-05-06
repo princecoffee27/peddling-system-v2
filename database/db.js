70e82205bc7aa3e234ea52de5299e1ed356374bc
princecoffee27 <princecoffee27@gmail.com>
2026-05-06 15:00:04 +0800

    Fix Railway MySQL transaction connection

--- database/db.js
+++ database/db.js
@@ -1,19 +1,14 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-    uri: process.env.MYSQL_URL,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
-db.getConnection((err, connection) => {
+db.connect((err) => {
     if (err) {
         console.error("DB CONNECTION ERROR:", err);
-    } else {
-        console.log("Connected to MySQL pool");
-        connection.release();
+        return;
     }
+
+    console.log("Connected to MySQL");
 });
 
 module.exports = db;
